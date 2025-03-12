bec70c0de3553e321c7c90237751054593eb010a
ErikDanielsson <danielsson.erik.0@gmail.com>
2025-03-12 17:00:03 +0100

    Start adding drift kernel tests

---
+++ nf-treeppl-tests/models/distributions/run_docker.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+model_dir=$1
+seed=$2
+niter=$3
+flag="-m mcmc-lightweight --cps partial --align --kernel --debug-phases"
+docker run --rm -v $PWD/$model_dir:/$model_dir danielssonerik/treeppl:custom-drift-kernels \
+    /bin/bash -c "tpplc /$model_dir/model.tppl --seed $seed $flag; ./out /$model_dir/data.json $niter" 
---
+++ nf-treeppl-tests/models/driftkernel/beta-uniform/compare.py
@@ -0,0 +1,14 @@
+import numpy as np
+import json
+import sys
+
+data_fn = sys.argv[1]
+niter = int(sys.argv[2])
+out_fn = sys.argv[3]
+
+with open(data_fn) as fh:
+    data_json = json.load(fh)
+
+betas = np.random.beta(data_json["a"], data_json["b"], niter)
+with open(out_fn, "w") as fh:
+    json.dump({"samples": {"x": betas.tolist()}}, fh)
